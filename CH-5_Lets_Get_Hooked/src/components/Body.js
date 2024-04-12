917de8fbe71a880d15d4d7156b4acb44238d53e7
narpat-makwana <narpatmakwana.4730@gmail.com>
2024-04-12 14:57:37 +0530

    Done Here

--- CH-5_Lets_Get_Hooked/src/components/Body.js
+++ CH-5_Lets_Get_Hooked/src/components/Body.js
@@ -1,20 +1,34 @@
 import Search from "./Search";
 import { restaurantList } from "../constants";
 import Card from "./Card";
+import React,{ useState } from "react";
+
 
 const Body = () => {
+
+  const[restList,setRestList] = useState(restaurantList);
+
     return (
       <>
         <div className="searchBar">
           <Search />
         </div>
+        <div className="filter">
+          <button className="filter-btn" onClick={()=>{
+           let filteredList = restaurantList.filter((res) => res.data.avgRating > 4);
+            // {console.log(filteredList)} 
+            setRestList(filteredList);
+          }}>Top Rated Restaurants</button>
+          
+        </div>
   
         <div className="body">
           
           {
-            restaurantList.map((restaurant) => {
+            restList.map((restaurant) => {
               return <Card key={restaurant.data.id} resData = {restaurant.data} />
             })
+
           }
          
         </div>
